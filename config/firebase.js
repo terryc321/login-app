990d4d7748329ce5519f3d41a8db4124fcfc4ba3
terry <terry.cadd@gmail.com>
2022-11-11 09:16:23 +0000

    added public api keys

--- config/firebase.js
+++ config/firebase.js
@@ -7,15 +7,15 @@ import Constants from 'expo-constants';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
 // Your web app's Firebase configuration
+// the apiKey is not secret , acceptable to be shared and on github repo
 const firebaseConfig = {
-  apiKey: Constants.manifest?.extra?.firebaseApiKey,
-  authDomain: Constants.manifest?.extra?.firebaseAuthDomain,
-  projectId: Constants.manifest?.extra?.firebaseProjectId,
-  storageBucket: Constants.manifest?.extra?.firebaseStorageBucket,
-  messagingSenderId: Constants.manifest?.extra?.firebaseMessagingSenderId,
-  appId: Constants.manifest?.extra?.firebaseAppId,
+  apiKey: "AIzaSyB0pr1FTrmTQ20qVjJLtQAiNvPKFCvjRLA",
+  authDomain: "demo1-10664.firebaseapp.com",
+  projectId: "demo1-10664",
+  storageBucket: "demo1-10664.appspot.com",
+  messagingSenderId: "947876007835",
+  appId: "1:947876007835:web:2348aeffd8eb0611f71b3e",
 };
 
 console.log("firebaseConfig = " , firebaseConfig);
